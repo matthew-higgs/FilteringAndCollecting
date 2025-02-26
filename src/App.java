3bf71c157016599fabbeeb0f5862c53ca409372e
Chewbacca <higgimat000@warren.k12.in.us>
2025-02-26 13:54:11 -0500

    Initial commit

--- src/App.java
+++ src/App.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class App {
+import java.util.stream.Stream;
+
+public class App {
+    public static void main(String[] args) {
+        var evens = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                .filter(t -> t % 2 == 0)
+                .toList();
+        System.out.println(evens);
+        
+
+    }
 }
